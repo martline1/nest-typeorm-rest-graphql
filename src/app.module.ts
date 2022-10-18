a4e5934e57b1f9581a6c14a0333e37d1e35ebab0
Martin Alcala <yael.alcalla@gmail.com>
2022-10-18 09:02:18 -0500

    feat: :sparkles: defined graphql and rest structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,43 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+
+// Import Own Modules
+import { UsersModule } from './components/users/users.module';
+import { BlogsModule } from './components/blogs/blogs.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot(),
+
+    // GraphQL Support
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+
+      // false in production
+      debug: true,
+      playground: true,
+    }),
+
+    // TypeORM Support (MySQL)
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'test',
+      autoLoadEntities: true,
+
+      // Don't forget to set synchronize to false in production
+      synchronize: true,
+    }),
+    UsersModule,
+    BlogsModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
